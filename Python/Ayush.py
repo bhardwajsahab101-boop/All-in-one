80ddb096081a7de382c24e1271199b9c19300ddf
bhardwajsahab101-boop <bhardwajsahab101@gmail.com>
2026-01-01 16:44:15 +0530

    Save local changes before rebase

--- Python/Ayush.py
+++ Python/Ayush.py
@@ -1,3 +1,30 @@
+"""File handling in python"""
+
+
+
+"""reading a file in python """
+
+# ayush = open("ayush.txt", "r")
+# content = ayush.read()
+# print(content)
+# ayush.close()
+
+
+
+
+"""writing a file in python """
+
+# ayush = open("ayush.txt", "w")
+# ayush.write("""
+# ayush is a good coder.
+# He loves to solve problems.
+# He is learning Python.
+# """)
+# ayush.close()
+
+
+"""Doing both reading and writing a file in python """
+
 # ayush = open("ayush.txt", "w")
 # ayush.write("""
 #             Ayush is a boy.
@@ -11,8 +38,12 @@
 # print(content)
 # ayush.close()
 
+
+"""doing python coding in different file and reading it in another file"""
+
 # ayush = open('test.py', 'w')
 # ayush.write("""# this is a test file\nprint("Hello, World!")\n
+
 # print("Hello, World!")
 # num1 = int(input())
 # num2 = int(input())
@@ -20,9 +51,9 @@
 # print("The sum is:", sum)
 # """)
 
-# read = open('test.py', 'r')
-# print(read.read())
+# ayush.close()
 
+"""creating and reading a new file in python"""
 
 
 # new = open("new.txt", "x")
@@ -30,5 +61,33 @@
 # new.close()
 # new = open("new.txt", "r")
 # print(new.read())
-# new.close()
+# new.close()'
 
+
+
+"""__________________________________________________________________________________________________________________"""
+
+
+
+"""learning classes in python"""
+
+# class Ayush :
+#     x = 5
+#     y = 10
+    
+#     summ = x + y
+#     pass
+# object1 = Ayush()
+# print(object1.summ)
+
+"""using __init__ method of class in python"""
+
+# class person:
+#     def __init__(self,name,age,city):
+#         self.name = name
+#         self.age = age  
+#         self.city = city
+# obj = person("ayush", 18,"gurugram")
+# print(obj.name)
+# print(obj.age)
+# print(obj.city)
